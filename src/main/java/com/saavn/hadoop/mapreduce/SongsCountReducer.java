1a8b8b285faabd3170aee3e7a277f1a5ac58f701
Nilanjan Sen <nsen@healthesystems.com>
2018-11-08 11:30:12 -0500

    added missing file

---
+++ src/main/java/com/saavn/hadoop/mapreduce/SongsCountReducer.java
@@ -0,0 +1,38 @@
+package com.saavn.hadoop.mapreduce;
+
+
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by nsen
+ */
+public class SongsCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+	private static final Logger logger = LoggerFactory.getLogger(SongsCountReducer.class);
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException {
+
+        int count = 0;
+        for (IntWritable value : values) {
+            count = count + value.get();
+        }
+
+        
+        try {
+			context.write(key, new IntWritable(count));
+		} catch (InterruptedException e) {
+			logger.error("SongsCountReducer.reduce() context.write() failed : " + e.getMessage());
+			e.printStackTrace();
+		}
+    }
+}
+
